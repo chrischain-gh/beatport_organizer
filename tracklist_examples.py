f8e4e406b0218a44894d1b566816db49f3ce5a2f
chrischain-gh <69068010+chrischain-gh@users.noreply.github.com>
2021-09-19 19:53:24 -0400

    Initial .py file commit

---
+++ tracklist_examples.py
@@ -0,0 +1,127 @@
+algorithm = "Line Split Only"
+tracklist_raw = """DJ Dainskin - Move Your Body
+Dave Angel - Funk Music (Dave Angel Rework)
+Ian Pooley - Gimmie Sound"""
+
+algorithm = "Line Split + \'# - Timestamp -\'"
+tracklist_raw = """01 - 00:00 - DJ Dainskin - Move Your Body
+02 - 05:21 - Dave Angel - Funk Music (Dave Angel Rework)
+03 - 09:24 - Ian Pooley - Gimmie Sound
+04 - 14:07 - X-Madman - Help
+05 - 18:17 - Ian Pooley presents Bluelite - Enlite
+06 - 23:33 - Dave Angel - Funk Music (DJ Tonka Mix)
+07 - 28:29 - Peace Division - D Tuned
+08 - 33:18 - Luke Slater - Stomp
+09 - 36:00 - DJ Restyles - Past Silence
+10 - 41:36 - Olav Basoski - Water
+11 - 44:48 - Mark Seven - Don't Let Me Down
+12 - 50:03 - Olav Basoski - Bring The Bass
+13 - 53:56 - Mark Seven - Gonz Gets FU
+14 - 56:22 - Jan Driver - Filter
+15 - 01:00:16 - Mark N-R-G - Don't Stop (Dave Angel Remix)
+16 - 01:04:45 - Kamaflarge - Discotamination (Dave Angel Rework)
+17 - 01:08:40 - Christian Smith vs Jean-Phillippe Aviance - Goldrush
+18 - 01:13:41 - Warped 69 - A High Naturally (Dave Angel Retake)
+01 - 01:18:54 - Spirit Guide - The First Few Steps
+02 - 01:23:20 - The Mulder - The Police
+03 - 01:26:23 - Jack Mackrel - Drum Tumbler
+04 - 01:28:50 - Velocity Boy - Triangular Funk
+05 - 01:32:41 - Bushflange - Foot Thought
+06 - 01:36:44 - Protein Boy - Wanna Go Bang
+07 - 01:40:22 - Olav Basoski - The Prize
+08 - 01:43:57 - Olav Basoski - Beats U Up
+09 - 01:46:58 - Olav Basoski - Keep On Dancin'
+10 - 01:49:26 - Ian Pooley - Loopduell 1 (On Guard Mix)
+11 - 01:52:26 - Dave Angel - This Is Disco (Dave Angel Remix)
+12 - 01:56:12 - Alexi Delano & Cari Lekebusch - Jimmy's Technique
+13 - 01:58:53 - 2 B Continued - Laydown (S'n'S Remix)
+14 - 02:03:43 - The Subjective - Tremmer
+15 - 02:05:57 - Mark Seven - Aftershock
+16 - 02:10:15 - Mark Seven - High
+17 - 02:14:43 - Nico Awtsventin - Jumping Jupiter
+18 - 02:18:07 - Green - Poison
+19 - 02:21:08 - Nico Awtsventin - Formulas
+20 - 02:26:00 - Adam Beyer - 4.50
+21 - 02:28:53 - Nico Awtsventin - Funx Patterns"""
+
+algorithm = "YouTube Standard"
+tracklist_raw = """Song
+Ending Point
+Artist
+Conrad Van Orton
+Album
+Hypersomnia
+Licensed to YouTube by
+dig dis (on behalf of Attic Music)
+Song
+Kinetic Lullaby (Pris Remix)
+Artist
+AWB
+Album
+Kinetic Lullaby
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Taapion)
+Song
+Articulation
+Artist
+Altinbas
+Album
+Articulation
+Licensed to YouTube by
+DeepMiningCorpAssoc (on behalf of Float Records); IRICOM
+Song
+Derzhprom (Keith Carnal Remix)
+Artist
+Opuswerk
+Album
+Social Condenser
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of ARTS); Muserk Rights Management, and 2 Music Rights Societies
+Song
+Black Swan
+Artist
+Dasha Rush
+Album
+Ain't No God Nor King
+Licensed to YouTube by
+DeepMiningCorpAssoc (on behalf of Sonic Groove)
+Song
+Script (I/Y Remix)
+Artist
+Sept
+Album
+Voice Of The Night Vol. 1
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Voxnox Records)
+Song
+In Basements (Original Mix)
+Artist
+Robert Hood
+Album
+Apartment Zero
+Licensed to YouTube by
+Zebralution, and 1 Music Rights Societies
+Song
+Citofono 1
+Artist
+CNCPT
+Album
+Evidence Based Vol. 4
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Triple Vision Music Group)
+Song
+You Are (Dark Mix)
+Artist
+Kenny Larkin
+Album
+You Are
+Licensed to YouTube by
+[Merlin] K7 Records (on behalf of Planet E Communications)
+Song
+Steady Pulse
+Artist
+Jeroen Search
+Album
+Endless Circles EP
+Licensed to YouTube by
+[Merlin] Was Wordandsound Medien GmbH (on behalf of Figure); Muserk Rights Management, and 2 Music Rights Societies"""
